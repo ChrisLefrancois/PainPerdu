959f26c11e4c49682a8e8ea458d4cf8effd87e35
Christopher Lefrancois <chris.lefrancois1@gmail.com>
2024-07-09 16:02:54 -0600

    started a basic navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-brand">
+        <Link to="/">MyApp</Link>
+      </div>
+      <div className="navbar-links">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Link to="/contact">Contact</Link>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
